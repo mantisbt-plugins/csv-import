b150c0a92b124cb439d0acc5ebe3f272d916a67f
Victor Boctor <victor@mantishub.com>
2014-06-09 23:01:21 -0700

    Update plugin version to 1.4.

--- Csv_import.php
+++ Csv_import.php
@@ -4,11 +4,11 @@ class Csv_importPlugin extends MantisPlugin
 	function register() {
 		$this->name = plugin_lang_get( 'title' );
 		$this->description = plugin_lang_get( 'description' );
-		$this->version = '1.3.0b';
+		$this->version = '1.4.0';
 		$this->requires = array( 'MantisCore' => '1.2.0' );
 		$this->author = 'Bug 4220 Team';
-		$this->contact = 'https://github.com/lionheart33806/Csv-import-4-MantisBT';
-		$this->url = 'https://github.com/lionheart33806/Csv-import-4-MantisBT';
+		$this->contact = 'https://github.com/mantisbt-plugins/csv-import/';
+		$this->url = 'https://github.com/mantisbt-plugins/csv-import/';
 		$this->page = 'config';
 	}
 
